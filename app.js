c992d79ab887d47e6e38f4736e38e1914ac6b5c9
Esteban Carbono <ecarbono@informatica-tr.com>
2022-03-10 12:48:38 -0500

    make it better

--- app.js
+++ app.js
@@ -1,17 +1,20 @@
 require("dotenv").config();
 const express = require("express");
+var bodyParser = require('body-parser')
+const { Pool, Client } = require('pg')
+const db = require('./queries')
 const app = express();
 const port = process.env.PORT;
 
+var jsonParser = bodyParser.json()
+var urlencodedParser = bodyParser.urlencoded({ extended: false })
 
-app.use(express.static("public"));
-
-app.get("api/care_pan", (req, res) => {
-    res.json({
-        "saludo": "Hola care pan"
-    });
-});
+app.get('/api/users', db.getUsers)
+app.get('/api/users/:id', db.getUserById)
+app.post('/api/cusers',jsonParser, db.createUser)
+app.put('/api/upusers/:id', db.updateUser)
+app.delete('/api/dlusers/:id', db.deleteUser)
 
 app.listen(port, () => {
-    console.log(`aplicacion corriendo en el puerto ${port}`);
+    console.log(`Aplicacion corriendo en el puerto ${port}`);
 })
---
+++ queries.js
@@ -0,0 +1,81 @@
+const Pool = require('pg').Pool
+const pool = new Pool({
+user: 'postgres',
+host: 'localhost',
+database: 'Movilizat',
+password: 'admin',
+port: 5432,
+})
+
+const getUsers = (request, response) => {
+  
+  pool.query('SELECT * FROM public.accounts', (error, results) => {
+    if (error) {
+      throw error
+    }
+    response.status(200).json(results.rows)
+  })
+}
+
+const getUserById = (request, response) => {
+  const id = parseInt(request.params.id)
+
+  pool.query('SELECT * FROM public.accounts WHERE user_id = $1', [id], (error, results) => {
+    if (error) {
+      throw error
+    }
+    response.status(200).json(results.rows)
+  })
+}
+
+const createUser = (request, response) => {
+  const {id,name,pass,email,create,last} = request.body
+  pool.query('INSERT INTO public.accounts (user_id,username,password,email,created_on,last_login) VALUES ($1, $2, $3, $4, $5 ,$6)', [id,name,pass ,email,create,last], (error, results) => {
+    if (error) {
+      response.status(201).send({"respon":"1",
+      "mensaje":error.message,
+    })
+    }else{
+      response.status(201).send({"respon":"0",
+      "mensaje":"Ok",
+    })
+    }
+    
+  })
+  
+}
+
+const updateUser = (request, response) => {
+  const id = parseInt(request.params.id)
+  const { name, email } = request.body
+
+  pool.query(
+    'UPDATE users SET name = $1, email = $2 WHERE id = $3',
+    [name, email, id],
+    (error, results) => {
+      if (error) {
+        throw error
+      }
+      response.status(200).send(`User modified with ID: ${id}`)
+    }
+  )
+}
+
+const deleteUser = (request, response) => {
+  const id = parseInt(request.params.id)
+
+  pool.query('DELETE FROM users WHERE id = $1', [id], (error, results) => {
+    if (error) {
+      throw error
+    }
+    response.status(200).send(`User deleted with ID: ${id}`)
+  })
+}
+
+module.exports = {
+  getUsers,
+  getUserById,
+  createUser,
+  updateUser,
+  deleteUser,
+}
