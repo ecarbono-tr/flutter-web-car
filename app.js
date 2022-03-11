741b4614a0f31a06ba6b36d24cb40d622a31a770
Esteban Carbono <ecarbono@informatica-tr.com>
2022-03-10 22:28:17 -0500

    make it better

--- app.js
+++ app.js
@@ -2,9 +2,9 @@ require("dotenv").config();
 const express = require("express");
 var bodyParser = require('body-parser')
 const { Pool, Client } = require('pg')
-const db = require('./queries')
 const app = express();
-
+const port = server.address().port;
+const db = require('./queries')
 
 var jsonParser = bodyParser.json()
 //var urlencodedParser = bodyParser.urlencoded({ extended: false })
@@ -16,6 +16,6 @@ app.put('/api/upusers/:id', db.updateUser)
 app.delete('/api/dlusers/:id', db.deleteUser)
 
 const server = app.listen(process.env.PORT || 5000, () => {
-    const port = server.address().port;
+    
     console.log(`Express is working on port ${port}`);
 });
