16ab24ff3b6d7867863f1e3073a44bda241948f3
Esteban <ecarbono@informatica-tr.com>
2022-03-31 16:06:42 -0500

    make it better

--- models/users.models.js
+++ models/users.models.js
@@ -1,26 +1,29 @@
 const { initDB } = require("../DB/connectDB");
-
+const client={};
 const getUsermodel = async (request) => {
     const { email, pass} = request.body
-    let result;
+    let result;    
     try {
-        const client = await initDB()
-        result = await client.query('SELECT * FROM public.cuentas WHERE email = $1 and password= $2;',[email,pass]);
+        client = await initDB()
+        result = await client.query('SELECT * FROM public.cuentas WHERE email = $1 and password= $2;',[email,pass]);        
     } catch (error) {
         result = error;
+        
     }
+    client.close()
     return result;
 }
 const setUsers = async (body) => {
     let result;
     try {
-        const client = await initDB()
+        client = await initDB()
         const values = [body.user_id, body.username, body.password, body.email, body.tipo_document, body.celular, body.rol, body.client, body.centro]
         result = await client.query('INSERT INTO public.cuentas (user_id, username, password, email, tipo_document, celular, rol, client, centro) VALUES ($1, $2, $3, $4, $5 ,$6,$7,$8,$9)', values);
         result = "0";
     } catch (error) {
         result = "1";
     }
+    client.close()
     return result;
 }
 
