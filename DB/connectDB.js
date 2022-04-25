16c8ffea2e99cea8b902290870d9a37c431cc2a5
Esteban Carbono <ecarbono@informatica-tr.com>
2022-04-25 12:33:27 -0500

    make it better

--- DB/connectDB.js
+++ DB/connectDB.js
@@ -1,8 +1,10 @@
 const { Client } = require('pg');
 
 const initDB = async () => {
-  
-  const conn ="movilizat://movilizat:movilizat@127.0.0.1:5432/movilizat";
+  //local: movilizat://movilizat:movilizat@127.0.0.1:5432/movilizat
+  //heroku postgres://fwwctcptkimwhj:5acad2bcfd0ee586128699536f60a19d60319170749cd939253e174e72e51d3f@ec2-3-229-161-70.compute-1.amazonaws.com:5432/dc0f2adbt4j2ji
+
+  const conn ="postgres://fwwctcptkimwhj:5acad2bcfd0ee586128699536f60a19d60319170749cd939253e174e72e51d3f@ec2-3-229-161-70.compute-1.amazonaws.com:5432/dc0f2adbt4j2ji";
   const client = new Client({
     connectionString: conn,
     ssl: false 
