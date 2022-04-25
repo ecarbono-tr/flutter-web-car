86e1870869a6f6ee57eab7f0af078e35667c844f
Esteban Carbono <ecarbono@informatica-tr.com>
2022-04-25 12:43:28 -0500

    make it better

--- DB/connectDB.js
+++ DB/connectDB.js
@@ -7,10 +7,10 @@ const initDB = async () => {
   const conn ="postgres://fwwctcptkimwhj:5acad2bcfd0ee586128699536f60a19d60319170749cd939253e174e72e51d3f@ec2-3-229-161-70.compute-1.amazonaws.com:5432/dc0f2adbt4j2ji";
   const client = new Client({
     connectionString: conn,
-    ssl: false 
-    // ssl: {
-    //   rejectUnauthorized: false
-    // }
+    //ssl: false 
+    ssl: {
+      rejectUnauthorized: false
+    }
   });
 
   await client.connect();
